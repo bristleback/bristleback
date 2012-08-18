bf3af7a0c79eeef04b779ac1945a1a41fd37e4d0
wojniemiec <woj.niemiec@gmail.com>
2012-08-18 14:16:51 +0200

    Bristleback namespace introduced, first tag: serverMessages added.

---
+++ src/main/java/pl/bristleback/server/bristle/conf/namespace/BristlebackNamespaceHandler.java
@@ -0,0 +1,16 @@
+package pl.bristleback.server.bristle.conf.namespace;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * //@todo class description
+ * <p/>
+ * Created on: 2012-08-18 08:59:12 <br/>
+ *
+ * @author Wojciech Niemiec
+ */
+public class BristlebackNamespaceHandler extends NamespaceHandlerSupport {
+  public void init() {
+    registerBeanDefinitionParser("serverMessages", new BristlebackServerMessagesBeanDefinitionParser());
+  }
+}
