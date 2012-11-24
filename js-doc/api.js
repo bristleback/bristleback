71e62cd1d69a218a73ebebecc9952cd84c9ba00d
wojniemiec <woj.niemiec@gmail.com>
2012-11-24 20:35:23 +0100

    javascript docs

---
+++ js-doc/api.js
@@ -0,0 +1,51 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Bristleback.CONNECTOR",
+        "Bristleback.Client",
+        "Bristleback.Console",
+        "Bristleback.LOCAL_HOSTNAME",
+        "Bristleback.controller.Action",
+        "Bristleback.controller.ActionCallback",
+        "Bristleback.controller.ActionClass",
+        "Bristleback.controller.ActionController",
+        "Bristleback.controller.ActionExceptionHandler",
+        "Bristleback.controller.ClientActionClass",
+        "Bristleback.utils"
+    ],
+    "modules": [
+        "Bristleback",
+        "controller",
+        "core",
+        "serialization",
+        "utils"
+    ],
+    "allModules": [
+        {
+            "displayName": "Bristleback",
+            "name": "Bristleback",
+            "description": "Action controller module"
+        },
+        {
+            "displayName": "controller",
+            "name": "controller",
+            "description": "Action controller module"
+        },
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "Core module"
+        },
+        {
+            "displayName": "serialization",
+            "name": "serialization",
+            "description": "Serialization module"
+        },
+        {
+            "displayName": "utils",
+            "name": "utils",
+            "description": "Utilities module"
+        }
+    ]
+} };
+});
