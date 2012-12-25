c14a3542f96d4c2409d3dab01376921432466330
wojniemiec <woj.niemiec@gmail.com>
2012-12-25 12:00:06 +0100

    #13 - some documentation

--- src/main/java/pl/bristleback/server/bristle/serialization/system/json/extractor/FormattingValueSerializer.java
+++ src/main/java/pl/bristleback/server/bristle/serialization/system/json/extractor/FormattingValueSerializer.java
@@ -1,6 +1,12 @@
 package pl.bristleback.server.bristle.serialization.system.json.extractor;
 
 /**
+ * Extended {@link ValueSerializer} interface, implementations should be able to handle <code>format</code> property
+ * of the {@link pl.bristleback.server.bristle.serialization.system.annotation.Bind},
+ * {@link pl.bristleback.server.bristle.serialization.system.annotation.Serialize} and
+ * {@link pl.bristleback.server.bristle.serialization.system.annotation.Property} annotations.
+ * Because it may be possible to have multiple threads using the same formatting objects,
+ * returned format representation must be thread safe.
  * <p/>
  * Created on: 16.12.12 14:40 <br/>
  *
@@ -8,5 +14,12 @@ package pl.bristleback.server.bristle.serialization.system.json.extractor;
  */
 public interface FormattingValueSerializer<T> extends ValueSerializer<T> {
 
+  /**
+   * Creates any kind of object that will store information about format used in given field.
+   * Returned format object MUST BE capable of performing formatting operations in multiple threads simultaneously.
+   *
+   * @param formatAsString format passed in serialization annotations.
+   * @return format representation or any object providing information about current format.
+   */
   Object prepareFormatHolder(String formatAsString);
 }
