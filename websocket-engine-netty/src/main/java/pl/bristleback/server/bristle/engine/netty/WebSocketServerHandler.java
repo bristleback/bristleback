328f09c4481cac167b97a8f6ac82d052bd8f3604
wojniemiec <woj.niemiec@gmail.com>
2013-05-12 18:47:57 +0200

    GENERAL - copyright in non-core modules

--- websocket-engine-netty/src/main/java/pl/bristleback/server/bristle/engine/netty/WebSocketServerHandler.java
+++ websocket-engine-netty/src/main/java/pl/bristleback/server/bristle/engine/netty/WebSocketServerHandler.java
@@ -1,17 +1,16 @@
 /*
- * Copyright 2010 Red Hat, Inc.
- *
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ * Bristleback Websocket Framework - Copyright (c) 2010-2013 http://bristleback.pl
+ * ---------------------------------------------------------------------------
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of merchantability
+ * or fitness for a particular purpose.
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+ * ---------------------------------------------------------------------------
  */
 package pl.bristleback.server.bristle.engine.netty;
 
