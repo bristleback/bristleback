110978d7aef7c704164c1008e370e1d84a3228eb
wojniemiec <woj.niemiec@gmail.com>
2013-05-12 18:59:07 +0200

    FIX - fixed build

--- bristleback-core/src/test/java/pl/bristleback/server/bristle/conf/resolver/serialization/SerializationInputResolverTest.java
+++ bristleback-core/src/test/java/pl/bristleback/server/bristle/conf/resolver/serialization/SerializationInputResolverTest.java
@@ -1,13 +1,12 @@
 package pl.bristleback.server.bristle.conf.resolver.serialization;
 
-import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import pl.bristleback.server.bristle.serialization.system.annotation.Bind;
 import pl.bristleback.server.bristle.serialization.system.SerializationInput;
+import pl.bristleback.server.bristle.serialization.system.annotation.Bind;
 import pl.bristleback.server.mock.action.SimpleActionClass;
 import pl.bristleback.server.mock.beans.SimpleMockBean;
 import pl.bristleback.server.mock.beans.SpringMockBeansFactory;
@@ -27,7 +26,6 @@ import static junit.framework.Assert.assertTrue;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/test-context.xml"})
 public class SerializationInputResolverTest {
-  private static Logger log = Logger.getLogger(SerializationInputResolverTest.class.getName());
 
   @Inject
   private SpringMockBeansFactory mockBeansFactory;
@@ -59,7 +57,7 @@ public class SerializationInputResolverTest {
     SerializationInput serializationInput = serializationInputResolver.resolveInputInformation(bindAnnotation);
 
     assertTrue(serializationInput.containsNonDefaultProperties());
-    assertTrue(serializationInput.getPropertyInformation().isRequired());
+    assertTrue(serializationInput.getNonDefaultProperties().get(SimpleMockBean.RAW_INT_PROPERTY).getPropertyInformation().isRequired());
 
   }
 }
