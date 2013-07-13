11a4ce15e72a5ab21b90c4b169e96a95c8ff9617
wojniemiec <woj.niemiec@gmail.com>
2013-07-13 21:35:37 +0200

    #19 - first working jackson serialization engine

--- serialization-engine-jackson/src/main/java/pl/bristleback/server/bristle/serialization/jackson/JacksonSerializationResolver.java
+++ serialization-engine-jackson/src/main/java/pl/bristleback/server/bristle/serialization/jackson/JacksonSerializationResolver.java
@@ -20,17 +20,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 import pl.bristleback.server.bristle.api.BristlebackConfig;
 import pl.bristleback.server.bristle.api.SerializationResolver;
-import pl.bristleback.server.bristle.serialization.SerializationResolvingException;
 import pl.bristleback.server.bristle.serialization.SerializationBundle;
-import pl.bristleback.server.bristle.serialization.system.SerializationInput;
-import pl.bristleback.server.bristle.serialization.system.annotation.Serialize;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Collection;
-import java.util.Map;
 
 /**
  * //@todo class description
@@ -56,55 +51,16 @@ public class JacksonSerializationResolver implements SerializationResolver<Jacks
 
   @Override
   public JacksonSerialization resolveSerialization(Type objectType, Annotation... annotations) {
-    return null;
-  }
-
-  //  @Override
-  public JacksonSerialization resolveDefaultSerialization(Type objectType) {
-    return resolveSerialization(objectType, new SerializationInput());
-  }
-
-  //  @Override
-  public JacksonSerialization resolveSerialization(Type objectType, SerializationInput input) {
     JacksonSerialization serialization = new JacksonSerialization();
     if (objectType instanceof ParameterizedType) {
       JavaType rootType = objectMapper.constructType(objectType);
       serialization.setType(rootType);
       return serialization;
     }
-    Class objectClass = (Class) objectType;
-    JavaType rootType;
-    if (Collection.class.isAssignableFrom(objectClass)) {
-      rootType = createCollectionSerialization(objectClass, input);
-    } else if (Map.class.isAssignableFrom(objectClass)) {
-      rootType = createMapSerialization(objectClass, input);
-    } else {
-      rootType = objectMapper.constructType(objectType);
-    }
+    JavaType rootType = objectMapper.getTypeFactory().constructType(objectType);
 
     serialization.setType(rootType);
     return serialization;
-  }
-
-  @SuppressWarnings("unchecked")
-  private JavaType createMapSerialization(Class objectClass, SerializationInput input) {
-    assertInputHasContainerElementType(input);
-    Class containerElementType = input.getPropertyInformation().getElementClass();
-    return objectMapper.getTypeFactory().constructMapType(objectClass, String.class, containerElementType);
-  }
-
-  @SuppressWarnings("unchecked")
-  public JavaType createCollectionSerialization(Class objectClass, SerializationInput input) {
-    assertInputHasContainerElementType(input);
-    Class containerElementType = input.getPropertyInformation().getElementClass();
-    return objectMapper.getTypeFactory().constructCollectionType(objectClass, containerElementType);
-  }
-
-  public void assertInputHasContainerElementType(SerializationInput input) {
-    if (input.getPropertyInformation().getElementClass().equals(Object.class)) {
-      throw new SerializationResolvingException("Error while resolving container element type, such information is not available. \n "
-        + "Provide parametrised type of container or specify container element type via " + Serialize.class.getSimpleName() + " annotation");
-    }
   }
 
   @Override
