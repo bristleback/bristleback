2bb7d2c6cbce93a0dda75ec46fe42a8d77ac58e3
wojniemiec <woj.niemiec@gmail.com>
2013-07-25 16:20:58 +0200

    GENERAL - changing version to 0.3.5

--- bristleback-core/src/main/java/pl/bristleback/server/bristle/app/BristlebackServerInstance.java
+++ bristleback-core/src/main/java/pl/bristleback/server/bristle/app/BristlebackServerInstance.java
@@ -31,7 +31,7 @@ import pl.bristleback.server.bristle.api.ServerEngine;
  */
 public class BristlebackServerInstance {
 
-  public static final String BRISTLEBACK_VERSION = "0.3.1";
+  public static final String BRISTLEBACK_VERSION = "0.3.5";
 
   public static final String BRISTLEBACK_HOMEPAGE = "http://bristleback.pl";
 
