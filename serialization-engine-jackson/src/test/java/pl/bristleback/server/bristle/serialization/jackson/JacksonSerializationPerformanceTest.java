6e570e7b5d2efeed71836a0696f7d8705a285be6
wojniemiec <woj.niemiec@gmail.com>
2013-07-28 19:02:55 +0200

    FIX - fixed compilation errors

--- serialization-engine-jackson/src/test/java/pl/bristleback/server/bristle/serialization/jackson/JacksonSerializationPerformanceTest.java
+++ serialization-engine-jackson/src/test/java/pl/bristleback/server/bristle/serialization/jackson/JacksonSerializationPerformanceTest.java
@@ -21,7 +21,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import pl.bristleback.server.bristle.message.BristleMessage;
+import pl.bristleback.common.serialization.message.BristleMessage;
 import pl.bristleback.server.mock.beans.VerySimpleMockBean;
 
 import java.util.*;
