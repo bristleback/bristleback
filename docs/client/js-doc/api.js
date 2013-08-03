3de8894096d20b0217753ee7f0303efdc954e71e
wojniemiec <woj.niemiec@gmail.com>
2013-08-03 20:17:55 +0200

    bristleback 0.3.5

---
+++ docs/client/js-doc/api.js
@@ -0,0 +1,67 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Bristleback.Client",
+        "Bristleback.LOCAL_HOSTNAME",
+        "Bristleback.USER_CONTEXT",
+        "Bristleback.USER_DETAILS",
+        "Bristleback.auth.SystemAuthentication",
+        "Bristleback.controller.Action",
+        "Bristleback.controller.ActionCallback",
+        "Bristleback.controller.ActionClass",
+        "Bristleback.controller.ActionController",
+        "Bristleback.controller.ActionExceptionHandler",
+        "Bristleback.controller.ActionMessage",
+        "Bristleback.controller.ClientActionClass",
+        "Bristleback.controller.controllers",
+        "Bristleback.template.TemplateController",
+        "Bristleback.utils"
+    ],
+    "modules": [
+        "Bristleback",
+        "auth",
+        "controller",
+        "core",
+        "serialization",
+        "template",
+        "utils"
+    ],
+    "allModules": [
+        {
+            "displayName": "auth",
+            "name": "auth",
+            "description": "Authentication module"
+        },
+        {
+            "displayName": "Bristleback",
+            "name": "Bristleback",
+            "description": "Authentication module"
+        },
+        {
+            "displayName": "controller",
+            "name": "controller",
+            "description": "Action controller module"
+        },
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "Core module"
+        },
+        {
+            "displayName": "serialization",
+            "name": "serialization",
+            "description": "Serialization module"
+        },
+        {
+            "displayName": "template",
+            "name": "template",
+            "description": "Template module"
+        },
+        {
+            "displayName": "utils",
+            "name": "utils",
+            "description": "Utilities module"
+        }
+    ]
+} };
+});
