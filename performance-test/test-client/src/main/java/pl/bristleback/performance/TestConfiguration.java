c6b5d41964f7c3bdf2dc6059d6abfba3f8ba2b6e
wojniemiec <woj.niemiec@gmail.com>
2013-11-11 23:51:56 +0100

    changed versions and manually added files

--- performance-test/test-client/src/main/java/pl/bristleback/performance/TestConfiguration.java
+++ performance-test/test-client/src/main/java/pl/bristleback/performance/TestConfiguration.java
@@ -1,11 +1,37 @@
 package pl.bristleback.performance;
 
-/**
- * Created with IntelliJ IDEA.
- * User: wojtek
- * Date: 11.11.13
- * Time: 23:35
- * To change this template use File | Settings | File Templates.
- */
 public class TestConfiguration {
+
+  private String host;
+  private int port;
+  private int clientNumber;
+  private int messagesNumber;
+
+  public TestConfiguration(String host, int port, int clientNumber, int messagesNumber) {
+    this.host = host;
+    this.port = port;
+    this.clientNumber = clientNumber;
+    this.messagesNumber = messagesNumber;
+  }
+
+  public String getHost() {
+    return host;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public int getClientNumber() {
+    return clientNumber;
+  }
+
+  public int getMessagesNumber() {
+    return messagesNumber;
+  }
+
+  @Override
+  public String toString() {
+    return "url: " + host + ":" + port + ", clients: " + clientNumber + ", total messages: " + messagesNumber;
+  }
 }
--- performance-test/test-client/src/main/java/pl/bristleback/performance/TestContext.java
+++ performance-test/test-client/src/main/java/pl/bristleback/performance/TestContext.java
@@ -1,11 +1,64 @@
 package pl.bristleback.performance;
 
-/**
- * Created with IntelliJ IDEA.
- * User: wojtek
- * Date: 11.11.13
- * Time: 23:35
- * To change this template use File | Settings | File Templates.
- */
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class TestContext {
+
+  private TestConfiguration configuration;
+
+  private List<TestWebsocketClient> clients;
+
+  private static final AtomicLong clientsConnected = new AtomicLong(0);
+  private static final AtomicLong messagesSent = new AtomicLong(0);
+  private static final AtomicLong messagesReceived = new AtomicLong(0);
+
+  public TestContext(TestConfiguration configuration) {
+    this.configuration = configuration;
+  }
+
+  public boolean allClientsAreConnected() {
+    return clients.size() == configuration.getClientNumber();
+  }
+
+  public int countConnectedClients() {
+    return clientsConnected.intValue();
+  }
+
+  public void clientConnected() {
+    clientsConnected.incrementAndGet();
+  }
+
+  public void clientDisconnected() {
+    clientsConnected.decrementAndGet();
+  }
+
+  public void messageSent() {
+    messagesSent.incrementAndGet();
+  }
+
+  public void messageReceived() {
+    messagesReceived.incrementAndGet();
+  }
+
+  public int countSentMessages() {
+    return messagesReceived.intValue();
+  }
+
+  public int countReceivedMessages() {
+    return messagesReceived.intValue();
+  }
+
+  public void setClients(List<TestWebsocketClient> clients) {
+    this.clients = clients;
+  }
+
+  public TestWebsocketClient getClient(int clientIndex) {
+    return clients.get(clientIndex);
+  }
+
+
+  public List<TestWebsocketClient> getAllClients() {
+    return clients;
+  }
 }
